b3ce2c2cd691e44f3e232ca8f9db05aa1248dbec
sebastianseannriley-cmyk <sebastianseannriley@gmail.com>
2025-12-07 16:41:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ import streamlit as st
 import altair as alt
 
 # ------------------------- Page Config -------------------------
+st.image("picsure.jpg")
 st.set_page_config(
     page_title="Car Price Portfolio (Interactive + Model X US Prices)",
     layout="wide",
