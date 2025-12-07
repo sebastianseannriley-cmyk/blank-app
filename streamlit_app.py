2a9d4738dce819f2352c8454fcb27246a4c3f1e9
sebastianseannriley-cmyk <sebastianseannriley@gmail.com>
2025-12-07 13:28:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
+import pandas as pd
 
-st.title("FAT BITCH")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+data = pd.read_csv("data.csv")
+st.write(data)
+
